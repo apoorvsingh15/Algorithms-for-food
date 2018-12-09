dbda1f33fca018c15ab9775b42c5f53d4e6d5a0e
Sarthak <sarthakn18@gmail.com>
2018-12-09 14:38:44 +0530

    Completed the code for dijkstra's algorithm

--- Algos-sarthak/Algorithms/Djikstra/README.md
+++ Algos-sarthak/Algorithms/Djikstra/README.md
@@ -1,3 +1,28 @@
 # Dijkstra's Algorithm
 
 This is a graph algorithm which helps us to find the shortest path from one node to every other node.
+
+Here is the pseudocode for this algorithm.
+
+* We initialize an object 'distances' where we initialize the distances of all the nodes from starting point
+to 999
+* We set the starting value to be 0 in the distances object
+* Then we create a priority queue in which the nodes nearest to the starting point will be put.
+* Till the priority queue is not empty follow these steps
+    * Remove the top value in the priority queue and store the top value in some variable.
+    * Get all the edges of the top value.
+    * If there are edges
+        * Add the weight of the top value to the weight of every vertex for each node you check.
+        * Iterate through the edges and get the node with the smallest weight.
+        * Store the node with the vertex with smallest weight in the priority queue.
+        * For all the nodes check if the distance value from the current node is less than what is stored, 
+            If yes then exchange
+* Return the 'distances' object.
+     
+
+For the graph
+
+[![solarized dualmode](https://github.com/apoorvsingh15/Algorithms-for-food/blob/master/assets/graph.jpg)](#features)
+
+The shortest distance of each of the nodes from the starting point are:
+> { A: 0, B: 3, C: 2, D: 5, E: 6 }
--- Algos-sarthak/Algorithms/Djikstra/dijkstra.js
+++ Algos-sarthak/Algorithms/Djikstra/dijkstra.js
@@ -5,39 +5,54 @@
 
 // Implementation
 
-function dijkstra(start, graph) {
+function dijkstra(start,graph){
+    let priorityQueue = [];
+    
     let nodes = graph.nodes.map(node => node.value);
-    let edges = graph.edges;
-    let adjacencies = graph.adjacencies;
+    let distances = {};
 
-    if (!nodes.includes(start)) {
-        console.log("The starting node was not found");
-    }
-    else {
+    nodes.forEach(node => distances[node] = 999);
+    
+    distances[start.value] = 0;
+    priorityQueue.push(start);
 
-        let startingEdges = edges[start];
-        if (startingEdges) {
-            let choose;
-            let weight = adjacencies[startingEdges[0]].weight;
-            for (let i = 0; i < startingEdges.length; i++) {
-                let adjacency = adjacencies[startingEdges[i]];
+    while (priorityQueue.length != 0){
+        let top = priorityQueue[0];
+        let currentWeight = top.weight;
 
-                if (weight >= adjacency.weight) {
-                    choose = adjacency
+        // Remove the top
+        priorityQueue = priorityQueue.slice(1,priorityQueue.length);
+
+        // Iterate over the edges of top
+        let edgesOfTop = graph.edges[top.value];
+
+        if (edgesOfTop && edgesOfTop.length != 0){
+            // For each edge find the weight and value
+            let firstEdge = graph.adjacencies[edgesOfTop[0]];
+    
+            let smallest = {
+                value: firstEdge.to,
+                weight: currentWeight + firstEdge.weight
+            }
+            
+            edgesOfTop.forEach(edgeVal => {
+                let edge = graph.adjacencies[edgeVal]
+                let totalWeight = currentWeight + edge.weight;
+                if (totalWeight < smallest.weight){
+                    smallest.value = edge.to;
+                    smallest.weight = totalWeight
                 }
-            }
-            console.log(choose);
-            if (choose.to === 'E') {
-                return choose;
-            } else {
-                return dijkstra(choose.to, graph);
-            }
+    
+                if (totalWeight < distances[edge.to]){
+                    distances[edge.to] = totalWeight;
+                }
+            });
+    
+            // Push the smallest value in the priority queue
+            priorityQueue.push(smallest);
         }
-        else{
-            return "no exit"
-        }
-        // console.log(edge);        
     }
+    return distances;
 }
 
 
@@ -46,12 +61,23 @@ function execute() {
     let graph = require('../../DataStructures/Graph/GraphImpl');
 
     // The value of the starting node
-    let start = 'A';
+    let start = {
+        value: 'A',
+        weight: 0
+    };
+
+    let mst;
 
     // Find the shortest path to all the other nodes using dijkstra's algorithm
-    let mst = dijkstra(start, graph);
-
-    console.log(mst);
+    let nodes = graph.nodes.map(node => node.value);
+    if (nodes.includes(start.value)){
+        mst = dijkstra(start, graph);
+        console.log("The distances of each of the nodes from the starting point are: ");
+        console.log(mst);
+    }
+    else {
+        console.log("Starting value is node a part of the graph");
+    }
 }
 
 module.exports = {
